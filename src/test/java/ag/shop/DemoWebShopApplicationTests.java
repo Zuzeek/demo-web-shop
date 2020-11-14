e17e51b118a3935f19bfa9aa675956960f09d8bb
Anna Groszek <121N@jdwilliamsgroup.com>
2020-11-14 12:39:20 +0000

    initial commit

---
+++ src/test/java/ag/shop/DemoWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ag.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
